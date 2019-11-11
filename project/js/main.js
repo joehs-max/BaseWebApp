2b6d193d8b53410d52c5e90be64c938f745a53a2
Joe Roche <jroche@dubmmfyd0znjk7q.hubspot.local>
2019-11-11 18:08:12 +0000

    With weather search

--- project/js/main.js
+++ project/js/main.js
@@ -1,4 +1,21 @@
+$(document).ready(function(){
+  getWeather();
+})
+function getWeather(searchquery) {
+  var url = "https://api.openweathermap.org/data/2.5/weather?q="+searchquery+"&units=imperial&APPID="+apiKey; 
 
+  $(".city").text("");
+  $(".temp").text("");
+  $(".error-message").text("");
+
+
+  $.ajax(url,{success: function(data){
+    $(".city").text(data.name);
+    $(".temp").text(data.main.temp);
+}, error: function(error){
+    $(".error-message").text("An error occured");
+}})
+}
 function myFunction() {
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
   // jQuery can do a lot of crazy stuff so make sure to google around to find out more
@@ -9,3 +26,7 @@ function myFunction() {
   $("#doge-image").append(`<img class="img-circle" src="images/wowdoge.jpeg" />`);
 }
 
+function searchWeather(){
+  var searchquery = $(".search").val();
+  getWeather(searchquery);
+}
